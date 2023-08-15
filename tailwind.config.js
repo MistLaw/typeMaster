2fb7e2fdabc96873f3c0556bf662eddc4c6e0481
BirdNoFly161 <sharp.oussama@gmail.com>
2023-08-15 15:39:19 +0100

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      primary: '#900C3F',
+      secondary: '#F8DE22',
+      accent: '#F94C10',
+      background: '#EAC696'
+    },  
+    extend: {},
+  },
+  plugins: [],
+}
+
